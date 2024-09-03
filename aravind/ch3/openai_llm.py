ea3f0b2c3ff183bb51fd518fceea42b1b3f2d1c9
Aravind Yarram <yaravind@gmail.com>
2024-09-02 20:05:16 -0400

    Print subscribed models

--- aravind/ch3/openai_llm.py
+++ aravind/ch3/openai_llm.py
@@ -1,3 +1,6 @@
+import openai
+import os
+
 from config import set_environment
 from langchain.llms import OpenAI
 
@@ -7,10 +10,21 @@ from langchain.agents import AgentType
 
 set_environment()
 
+api_key = os.getenv("OPENAI_API_KEY")
+
+# List available models
+openai.api_key = api_key
+models = openai.Model.list()
+
+# Print model IDs
+for model in models['data']:
+    print(model['id'])
+
 tools = load_tools(["python_repl"])
 responses = ["Action: Python_REPL\nAction Input: print(2 + 3)", "Final Answer: 4"]
-openai_llm = OpenAI(temperature=0., model_name="gpt-4o-mini")
+
+openai_llm = OpenAI(temperature=0., model_name="gpt-4o-mini", openai_api_key=api_key)
 
 agent = initialize_agent(tools, openai_llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-agent.run("whats 2 + 2")
-
+resp = agent.run("whats 2 + 2")
+print("--->" + resp)
